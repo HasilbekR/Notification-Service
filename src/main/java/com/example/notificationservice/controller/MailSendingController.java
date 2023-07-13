3caa3a0b5db12f2e779f21a50aedf6db10ecf838
javohirjahonov <jahonovjavohir0@gmail.com>
2023-07-13 17:13:08 +0500

    Added NotificationServiceApplication and other configurations

--- src/main/java/com/example/notificationservice/controller/MailSendingController.java
+++ src/main/java/com/example/notificationservice/controller/MailSendingController.java
@@ -1,4 +1,24 @@
 package com.example.notificationservice.controller;
 
+import com.example.notificationservice.dto.MailDto;
+import com.example.notificationservice.service.MailSendingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notification")
+@RequiredArgsConstructor
 public class MailSendingController {
+
+    private final MailSendingService mailSendingService;
+
+    @PostMapping("send-single")
+    public void sendMessage(
+            @RequestBody MailDto mailDto
+    ) {
+       mailSendingService.sendMessage(mailDto);
+    }
 }
